ceee31dba07642a682a05647327a843facfd918e
zarguell <zac.arguelles@gmail.com>
2022-06-20 22:30:57 -0400

    fixed argument parsing and json sinking

--- backfill.sh
+++ backfill.sh
@@ -4,7 +4,7 @@
 d=2021-11-01
 
 #get the kevs
-kevs=$(curl -s 'https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json')
+curl --silent --output cisa-kev-latest.json https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json
 
 # increment until date
 while [ "$d" != 2022-06-23 ]; do 
@@ -12,10 +12,10 @@ while [ "$d" != 2022-06-23 ]; do
   d=$(date -I -d "$d + 1 day")
 
 #backfill the dues and the news
-  echo "$kevs" | jq -r --arg d "$d" '.vulnerabilities[] | select(.dueDate==$d)' > vulns-due/$d-cisa-kev-due.json
-  echo "$kevs" | jq -r --arg d "$d" '.vulnerabilities[] |  select(.dateAdded==$d)' > vulns-added/$d-cisa-kev-new.json
+  cat cisa-kev-latest.json | jq --arg d "$d" '.vulnerabilities[] | select(.dueDate==$d)' | jq -s . - > vulns-due/$d-cisa-kev-due.json
+  cat cisa-kev-latest.json | jq --arg d "$d" '.vulnerabilities[] |  select(.dateAdded==$d)' | jq -s . - > vulns-added/$d-cisa-kev-new.json
 
 done
 
 #clean empties
-find ./ -size 0 -print -delete
+find ./vulns-* -type f -size -4c -delete
